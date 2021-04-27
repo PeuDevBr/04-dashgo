d5f42bc6fd8690c8dd9ada26c9e7bf071bfe93ac
Flávio Renato <peudevbr@gmail.com>
2021-04-26 21:29:34 -0300

    Adicionando MirageJS

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,21 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -9,26 +25,34 @@ export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },[]);
 
   return (
-    <Box >
-      <Header/>
+    <Box>
+      <Header />
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar/>
-            {/* flex="1" - ocupa toda a largura possível */}
+        <Sidebar />
+        {/* flex="1" - ocupa toda a largura possível */}
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
-            
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
             <Link href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
                 fontSize="sm"
-                colorScheme="pink"              
-                leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
@@ -39,22 +63,24 @@ export default function UserList() {
             <Thead>
               <Tr>
                 <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink"/>
+                  <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
                 {isWideVersion && <Th>Data de Cadastro</Th>}
-               {/* <Th width="8"/> */}
+                {/* <Th width="8"/> */}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
                 <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink"/>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Flávio Renato</Text>
-                    <Text fontSize="sm" color="gray.300">renatoarapiraca.82@gmail.com</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      renatoarapiraca.82@gmail.com
+                    </Text>
                   </Box>
                 </Td>
                 {isWideVersion && <Td>11 de Abril, 2021</Td>}
@@ -73,12 +99,14 @@ export default function UserList() {
 
               <Tr>
                 <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink"/>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Flávio Renato</Text>
-                    <Text fontSize="sm" color="gray.300">renatoarapiraca.82@gmail.com</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      renatoarapiraca.82@gmail.com
+                    </Text>
                   </Box>
                 </Td>
                 {isWideVersion && <Td>11 de Abril, 2021</Td>}
@@ -97,12 +125,14 @@ export default function UserList() {
 
               <Tr>
                 <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink"/>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Flávio Renato</Text>
-                    <Text fontSize="sm" color="gray.300">renatoarapiraca.82@gmail.com</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      renatoarapiraca.82@gmail.com
+                    </Text>
                   </Box>
                 </Td>
                 {isWideVersion && <Td>11 de Abril, 2021</Td>}
@@ -121,10 +151,9 @@ export default function UserList() {
             </Tbody>
           </Table>
 
-          <Pagination/>
+          <Pagination />
         </Box>
       </Flex>
-
     </Box>
   );
 }
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,49 @@
+import { createServer, Factory, Model } from "miragejs";
+import faker from "faker";
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+      // user do tipo User, <Partial> user não precisa ter todos os campos de User
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 200);
+    },
+
+    routes() {
+      this.namespace = "api"; // api/users - para acessar as rotas
+      this.timing = 750; // para cada chamada demorar 750ms para testar os carregamentos
+
+      this.get("/users");
+      // 'this.get()' - quando acessar a rota '/users' retorna toda a lista de usuários 'user'
+      this.post("/users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
